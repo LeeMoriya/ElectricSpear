34d9e0163fc7b641b6d4398ef09e0fc90e9b8108
LeeMoriya <leecassie12@gmail.com>
2021-11-18 07:03:38 +0000

    .

---
+++ AbstractElectricSpear.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class AbstractElectricSpear : AbstractPhysicalObject
+{
+	public int stuckInWallCycles;
+	public bool stuckVertically;
+	public int charge;
+
+	public AbstractElectricSpear(World world, ElectricSpear realizedObject, WorldCoordinate pos, EntityID ID, int charge) : base(world, (AbstractPhysicalObject.AbstractObjectType)31, realizedObject, pos, ID)
+	{
+		this.charge = charge;
+	}
+
+	public bool stuckInWall
+	{
+		get
+		{
+			return this.stuckInWallCycles != 0;
+		}
+	}
+
+	public void StuckInWallTick(int ticks)
+	{
+		bool flag = this.stuckInWallCycles > 0;
+		if (flag)
+		{
+			this.stuckInWallCycles = Math.Max(0, this.stuckInWallCycles - ticks);
+		}
+		else
+		{
+			bool flag2 = this.stuckInWallCycles < 0;
+			if (flag2)
+			{
+				this.stuckInWallCycles = Math.Min(0, this.stuckInWallCycles + ticks);
+			}
+		}
+	}
+
+	public override string ToString()
+	{
+		return string.Concat(new object[]
+		{
+				this.ID.ToString(),
+				"<oA>",
+				this.type.ToString(),
+				"<oA>",
+				this.pos.room,
+				".",
+				this.pos.x,
+				".",
+				this.pos.y,
+				".",
+				this.pos.abstractNode,
+				"<oA>",
+				this.stuckInWallCycles.ToString(),
+				"<oA>",
+				this.charge
+		});
+	}
+}
--- ElectricSpearPlugin.cs
+++ ElectricSpearPlugin.cs
@@ -16,6 +16,6 @@ public class ElectricSpearPlugin : BaseUnityPlugin
 
     public void OnEnable()
     {
-        
+
     }
 }

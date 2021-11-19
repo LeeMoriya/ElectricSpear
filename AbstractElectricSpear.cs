4e5f080e854e991a4f579393673e771eadf2ec15
LeeMoriya <leecassie12@gmail.com>
2021-11-19 17:08:39 +0000

    Revert "Revert "PlaceInRoom freeze fixed""
    This reverts commit 729f71b6768ff2414a6ff00b35cce802f5904238.

--- AbstractElectricSpear.cs
+++ AbstractElectricSpear.cs
@@ -4,18 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-public class AbstractElectricSpear : AbstractPhysicalObject
+public class AbstractElectricSpear : AbstractSpear
 {
-    public int stuckInWallCycles;
-    public bool stuckVertically;
     public int charge;
 
-    public AbstractElectricSpear(World world, ElectricSpear realizedObject, WorldCoordinate pos, EntityID ID, int charge) : base(world, ElectricSpearFisob.Instance.Type, realizedObject, pos, ID)
+    public AbstractElectricSpear(World world, ElectricSpear realizedObject, WorldCoordinate pos, EntityID ID, int charge) : base(world, realizedObject, pos, ID, false)
     {
+        this.type = ElectricSpearFisob.Instance.Type;
         this.charge = charge;
     }
 
-    public bool stuckInWall
+    public bool StuckInWall
     {
         get
         {
@@ -27,18 +26,6 @@ public class AbstractElectricSpear : AbstractPhysicalObject
     {
         realizedObject ??= new ElectricSpear(this, this.world);
         base.Realize();
-    }
-
-    public void StuckInWallTick(int ticks)
-    {
-        if (this.stuckInWallCycles > 0)
-        {
-            this.stuckInWallCycles = Math.Max(0, this.stuckInWallCycles - ticks);
-        }
-        else if (this.stuckInWallCycles < 0)
-        {
-            this.stuckInWallCycles = Math.Min(0, this.stuckInWallCycles + ticks);
-        }
     }
 
     public override string ToString()
